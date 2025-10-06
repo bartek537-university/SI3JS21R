90a93b86958dacc04ea6635eef6bef396848bb14
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2025-10-06 19:02:35 +0200

    Change result type annotation from Complex to complex for problem python/04

--- python/04_negative_number_sqrt.py
+++ python/04_negative_number_sqrt.py
@@ -1,8 +1,7 @@
-from numbers import Complex
 from typing import Final
 
 NEGATIVE_NUMBER: Final[int] = -23
 ROOT_ORDER = 2
 
-result: Complex = NEGATIVE_NUMBER ** (1 / ROOT_ORDER)
+result: complex = NEGATIVE_NUMBER ** (1 / ROOT_ORDER)
 print(f"{result.real} + {result.imag}i")
