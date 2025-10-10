f69e6099a23e59510772f39d18c026ce4f05d90f
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2025-10-10 12:40:53 +0200

    Solve problem python/19 - get random numbers and elements

---
+++ python/19_random_range_choice.py
@@ -0,0 +1,17 @@
+import random
+
+a = int(input("Podaj początek przedziału: "))
+b = int(input("Podaj koniec przedziału: "))
+
+random_number = random.randrange(a, b + 1)
+
+file_name = input("Podaj nazwę pliku: ")
+
+with open(file_name) as input_file:
+    file_contents = input_file.readlines()
+
+random_element = random.choice(file_contents)
+
+with open("wynikzadanie.txt", 'w+') as output_file:
+    output_file.write(f"{random_number}\n")
+    output_file.write(f"{random_element}\n")
