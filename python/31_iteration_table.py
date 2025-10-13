253d4ff73af15fa85ce5e4015252b8cae2b73d6e
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2025-10-13 20:02:00 +0200

    Solve problem python/31 - write iteration analysis to file

---
+++ python/31_iteration_table.py
@@ -0,0 +1,51 @@
+from io import TextIOWrapper
+from typing import Final
+
+output_file_name = input("Podaj nazwę pliku: ")
+iterations = int(input("Podaj ilość iteracji: "))
+value_delta = int(input("Podaj podstawę iteracji: "))
+
+COLUMN_WIDTH: Final[int] = 5
+COLUMN_FILL_CHAR: Final[str] = "*"
+HORIZONTAL_SPACER_EMPTY = "   "
+HORIZONTAL_SPACER_DASH = " | "
+
+
+def align_column_center(text: str) -> str:
+    return text.center(COLUMN_WIDTH, COLUMN_FILL_CHAR)
+
+
+def align_column_left(text: str) -> str:
+    return text.ljust(COLUMN_WIDTH, " ")
+
+
+def print_column_headers(writer: TextIOWrapper) -> None:
+    nominator_header = align_column_left("liczn.")
+    sum_header = align_column_left("suma")
+    writer.write(f"{nominator_header} {HORIZONTAL_SPACER_EMPTY} {sum_header}\n")
+
+
+def print_column_horizontal_line(writer: TextIOWrapper) -> None:
+    decoration = "-" * COLUMN_WIDTH
+    writer.write(f"{decoration} {HORIZONTAL_SPACER_EMPTY} {decoration}\n")
+
+
+def print_iteration_table_body(iteration: int, total: int, writer: TextIOWrapper) -> None:
+    iteration_text = align_column_center(str(iteration))
+    total_text = align_column_center(str(total))
+
+    horizontal_spacer = HORIZONTAL_SPACER_EMPTY if iteration <= 0 else HORIZONTAL_SPACER_DASH
+    writer.write(f"{iteration_text} {horizontal_spacer} {total_text}\n")
+
+
+with open(output_file_name, "w+") as file:
+    value_total = 0
+
+    print_column_headers(file)
+    for iteration in range(iterations):
+        print_iteration_table_body(iteration, value_total, file)
+
+        if iteration == 0:
+            print_column_horizontal_line(file)
+
+        value_total += value_delta
