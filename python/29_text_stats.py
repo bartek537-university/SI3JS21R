134acb87e07715f28bf33fd9d1c698c7e5772975
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2025-10-13 16:29:20 +0200

    Solve problem python/29 - calculate word statistics

---
+++ python/29_text_stats.py
@@ -0,0 +1,21 @@
+import re
+
+word_counts = dict[str, int]()
+
+
+def count_words(text: str, counts: dict[str, int]) -> None:
+    for word in re.findall(r"\w+", text):
+        lowercase_word = word.lower()
+
+        if lowercase_word not in word_counts:
+            word_counts[lowercase_word] = 0
+
+        word_counts[lowercase_word] += 1
+
+
+with open("tekstdostatystyki.txt") as file:
+    for line in file.readlines():
+        count_words(line, word_counts)
+
+for word in sorted(word_counts.keys()):
+    print(f"{word}: {word_counts[word]} razy")
