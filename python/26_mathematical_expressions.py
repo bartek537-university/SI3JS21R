0f2d2126f7d924eb8945f170670b8048b51d88d9
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2025-10-13 14:30:43 +0200

    Solve problem python/26 - mathematical formulas

---
+++ python/26_mathematical_expressions.py
@@ -0,0 +1,48 @@
+import math
+
+
+def task_a_plus(alfa: float, beta: float) -> float:
+    return 2 * math.sin((alfa + beta) / 2) * math.cos((alfa - beta) / 2)
+
+
+def task_a_minus(alfa: float, beta: float) -> float:
+    return 2 * math.sin((alfa - beta) / 2) * math.cos((alfa + beta) / 2)
+
+
+def task_b(x: float, n: int) -> float:
+    current_word = 1
+    binomial_sum = current_word
+
+    for i in range(n):
+        current_word *= (n - i) * x / (i + 1)
+        binomial_sum += current_word
+
+    return binomial_sum
+
+
+def task_c(a: float, b: float, c: float) -> float:
+    delta = b ** 2 - 4 * a * c
+    return (-b - math.sqrt(delta)) / (2 * a)
+
+
+def task_d(x: float, iterations: int = 100) -> float:
+    current_word = 1
+    e_sum = current_word
+
+    for i in range(iterations):
+        current_word *= x / (i + 1)
+        e_sum += current_word
+
+    return e_sum
+
+
+def task_e(arg_as: list[float], arg_bs: list[float], x: float, l: float, iterations: int = 100) -> float:
+    total = arg_as[0]
+
+    for n in range(1, iterations):
+        angle = n * math.pi * x / l
+        total += arg_as[n] * math.cos(angle) + arg_bs[n] * math.sin(angle)
+
+    return total
+
+print(task_e([1,2,3,4,5], [1,2,3,4,5], 1, 1, 5))
