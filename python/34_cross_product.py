4b5fcda8d03d179aade15af08803cd486a01bccf
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2025-10-13 21:39:22 +0200

    Solve problem python/34 - cross product

---
+++ python/34_cross_product.py
@@ -0,0 +1,25 @@
+from typing import Any, Generator
+
+genders = ["Kobieta", "Mężczyzna"]
+colors = ["Biały", "Czarny", "Zielony", "Czerwony", "Niebieski", "Żółty", "Szary"]
+sizes = ["XXL", "XL", "L", "M", "S", "XS"]
+
+
+def cross_product(*arrays: list[Any]) -> Generator[list[...]]:
+    empty_result = [None for _ in range(len(arrays))]
+    return _cross_product(empty_result, 0, *arrays)
+
+
+def _cross_product(current_result: list[...], array_index: int, *arrays: list[Any]) -> Generator[list[...]]:
+    if array_index >= len(arrays):
+        yield current_result
+        return
+
+    for value in arrays[array_index]:
+        current_result[array_index] = value
+        yield from _cross_product(current_result, array_index + 1, *arrays)
+
+
+for index, label in enumerate(cross_product(genders, colors, sizes)):
+    with open(f"34_metki/metka_{index}.txt", "w+") as label_file:
+        label_file.write(" ".join(label) + "\n")
