180ce43b8a7434bc547df1c911b132517813dd29
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2025-10-14 00:37:34 +0200

    Solve problem python/38 - implement common sorting algorithms

---
+++ python/38_sorting.py
@@ -0,0 +1,54 @@
+import random
+
+
+def bubble_sort(array: list[...]) -> None:
+    for i in range(len(array)):
+        for j in range(len(array) - i - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+
+
+def insertion_sort(array: list[...]) -> None:
+    for i in range(1, len(array)):
+        current_value = array[i]
+        j = i
+
+        while j >= 1 and array[j - 1] > current_value:
+            array[j] = array[j - 1]
+            j -= 1
+
+        array[j] = current_value
+
+
+def quick_sort(array: list[...], start: int, end: int) -> None:
+    if end - start < 1:
+        return
+
+    pivot_index = end - 1
+    pivot_value = array[pivot_index]
+
+    partition_index = start
+    for i in range(start, end - 1):
+        if array[i] < pivot_value:
+            array[i], array[partition_index] = array[partition_index], array[i]
+            partition_index += 1
+
+    array[pivot_index], array[partition_index] = array[partition_index], array[pivot_index]
+
+    quick_sort(array, start, partition_index)
+    quick_sort(array, partition_index + 1, end)
+
+
+a = [6, 3, 6, 2, 7, 3, 6, 9, 34, -19, 3, 7, 3, 56, 235]
+
+random.shuffle(a)
+bubble_sort(a)
+print(a)
+
+random.shuffle(a)
+insertion_sort(a)
+print(a)
+
+random.shuffle(a)
+quick_sort(a, 0, len(a))
+print(a)
