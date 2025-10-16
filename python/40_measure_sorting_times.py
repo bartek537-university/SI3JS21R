f7633dc5e6c536a9dd0c744717528c49a4c6b009
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2025-10-16 20:23:25 +0200

    Solve problem python/40 - measure sorting times and save results in a table

---
+++ python/40_measure_sorting_times.py
@@ -0,0 +1,103 @@
+import statistics
+from datetime import timedelta, datetime
+from io import TextIOWrapper
+from typing import Final, Callable
+
+from python.sorting_algorithms import bubble_sort, insertion_sort, quick_sort
+
+
+def load_numbers_from_file(file_name: str) -> list[float]:
+    with open(file_name) as file:
+        return [float(line) for line in file]
+
+
+UNSORTED_NUMBERS_FILE_NAMES: Final[list[str]] = [
+    "40_unsorted_a.txt",
+    "40_unsorted_b.txt",
+    "40_unsorted_c.txt",
+    "40_unsorted_d.txt",
+]
+
+unsorted_numbers: list[list[float]] = []
+
+for unsorted_numbers_file_name in UNSORTED_NUMBERS_FILE_NAMES:
+    unsorted_numbers.append(load_numbers_from_file(unsorted_numbers_file_name))
+
+
+class BenchmarkResult:
+    def __init__(self, algorithm_name: str, measurements: list[timedelta]):
+        self.algorithm_name = algorithm_name
+        self.measurements_seconds = BenchmarkResult._convert_measurements_to_seconds(measurements)
+
+    @staticmethod
+    def _convert_measurements_to_seconds(measurements: list[timedelta]) -> list[float]:
+        return [measurement.total_seconds() for measurement in measurements]
+
+    def __len__(self):
+        return len(self.measurements_seconds)
+
+    def get_mean_time_seconds(self) -> float:
+        return statistics.mean(self.measurements_seconds)
+
+    def get_standard_deviation_seconds(self) -> float:
+        return statistics.stdev(self.measurements_seconds)
+
+
+def write_file_line(file_writer: TextIOWrapper, values: list[...], delimiter: str = " ") -> None:
+    file_writer.write(f"{delimiter.join(map(str, values))}\n")
+
+
+COLUMN_DELIMITER: Final[str] = " -- "
+
+
+def file_write_benchmark_results(file_writer: TextIOWrapper, results: list[BenchmarkResult]) -> None:
+    write_file_line(file_writer, [
+        "NAZWA",
+        "LICZBA SORTOWANYCH PLIKÓW",
+        "ŚREDNI CZAS",
+        "ODCHYLENIE STANDARDOWE"
+    ], COLUMN_DELIMITER)
+
+    for index, result in enumerate(results):
+        write_file_line(file_writer, [
+            f"{index + 1}. {result.algorithm_name}",
+            len(result),
+            f"{result.get_mean_time_seconds()} s",
+            f"{result.get_standard_deviation_seconds()} s",
+        ], COLUMN_DELIMITER)
+
+
+Algorithm = tuple[str, Callable[[list[...]], None]]
+
+ALGORITHMS: Final[list[Algorithm]] = [
+    ("bubble_sort", lambda numbers: bubble_sort(numbers)),
+    ("insertion_sort", lambda numbers: insertion_sort(numbers)),
+    ("quick_sort", lambda numbers: quick_sort(numbers, 0, len(numbers))),
+]
+
+benchmark_results: list[BenchmarkResult] = []
+
+
+def measure_time(fn: Callable[[], None]) -> timedelta:
+    start_time = datetime.now()
+    fn()
+    end_time = datetime.now()
+    return end_time - start_time
+
+
+for name, algorithm in ALGORITHMS:
+    measured_times = [measure_time(lambda: algorithm(numbers)) for numbers in unsorted_numbers]
+    benchmark_results.append(BenchmarkResult(name, measured_times))
+
+FULL_DATETIME_FORMAT: Final[str] = "%Y%m%d%H%M%S"
+
+
+def get_report_file_name(timestamp: datetime) -> str:
+    formatted_timestamp = timestamp.strftime(FULL_DATETIME_FORMAT)
+    return f"raport_{formatted_timestamp}.txt"
+
+
+report_file_name = get_report_file_name(datetime.now())
+with open(report_file_name, "w+") as report_file:
+    report_file.write("RAPORT ALGORYTMÓW SORTOWANIA\n")
+    file_write_benchmark_results(report_file, benchmark_results)
