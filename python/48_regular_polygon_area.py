ca9600cf28016ccd278cc3e2e9df0f1b1cdb2465
Bartosz Bieniek <60577942+bartek537@users.noreply.github.com>
2025-10-19 02:13:21 +0200

    Solve problem python/48 - calculate regular polygon area

---
+++ python/48_regular_polygon_area.py
@@ -0,0 +1,8 @@
+import math
+
+side_count, side_length = map(int, input().split(", "))
+
+single_triangle_area = side_length ** 2 / (4 * math.tan(math.pi / side_count))
+regular_polygon_area = side_count * single_triangle_area
+
+print(regular_polygon_area)
